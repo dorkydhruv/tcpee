95504b8e37572c1fb768109e4a31acaec2c4f3c9
dorkydhruv <thedhruvsharma@gmail.com>
2026-03-18 23:53:45 +0530

    add ref

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+// ref: https://blog.netherlabs.nl/articles/2009/01/18/the-ultimate-so_linger-page-or-why-is-my-tcp-not-reliable
 fn main() {
     let mut remote: libc::sockaddr_in = unsafe { std::mem::zeroed() };
     remote.sin_family = libc::AF_INET as u8;
